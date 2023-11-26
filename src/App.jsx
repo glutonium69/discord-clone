3ee876126c5b2b23465fa0c517ec59529f775af8
glutonium69 <tahzib404@gmail.com>
2023-11-26 21:14:41 +0600

    Importing components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Servers from "./Components/Servers/Servers";
+import Dm from "./Components/Dm/Dm";
+import Main from "./Components/Main/Main";
+
 function App() {
 
-  return(
-    <div className="wrapper">
-        
-    </div>
-  );
+  return (
+		<div className="wrapper">
+			<Servers />
+			<Dm />
+			<Main />
+		</div>
+	);
 }
 
 export default App
