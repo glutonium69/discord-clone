5d86a8116afb0be1de229185c848714a23449dd2
glutonium69 <tahzib404@gmail.com>
2023-11-27 07:15:43 +0600

    correct naming to before ListItems to now ListItem

--- src/Components/Dm/List/ListItem.jsx
+++ src/Components/Dm/List/ListItem.jsx
@@ -1,16 +1,16 @@
 
 
-export default function ListItems() {
+export default function ListItems({icon, text}) {
 
     return (
 			<li>
 				<div className="img">
-					<img
+					{icon || <img
 						src="https://cdn.discordapp.com/avatars/492543441826349066/42c165aa65a0695ad140866b425bf9c3.webp?size=48"
 						alt=""
-					/>
+					/>}
 				</div>
-				<div className="name">He is spiderman</div>
+				<div className="name">{text || "He is spiderman"}</div>
 			</li>
 		);
 }
