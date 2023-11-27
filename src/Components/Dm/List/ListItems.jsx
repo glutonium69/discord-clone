4974ca2a37ed8a978d446bbe10e9f7df8a6d0841
glutonium69 <tahzib404@gmail.com>
2023-11-27 06:07:07 +0600

    Made a new component fot dm list item

---
+++ src/Components/Dm/List/ListItems.jsx
@@ -0,0 +1,16 @@
+
+
+export default function ListItems() {
+
+    return (
+			<li>
+				<div className="img">
+					<img
+						src="https://cdn.discordapp.com/avatars/492543441826349066/42c165aa65a0695ad140866b425bf9c3.webp?size=48"
+						alt=""
+					/>
+				</div>
+				<div className="name">He is spiderman</div>
+			</li>
+		);
+}
