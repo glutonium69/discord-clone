bf907d9b4e29bc98ee9f99c6d3bb291576e3bfe1
glutonium69 <tahzib404@gmail.com>
2023-11-28 00:05:06 +0600

    Updating the ListItem.jsx

--- src/Components/Dm/List/ListItem.jsx
+++ src/Components/Dm/List/ListItem.jsx
@@ -1,16 +1,15 @@
-
-
-export default function ListItems({icon, text}) {
+export default function ListItems({icon, text, appearance}) {
 
     return (
 			<li>
-				<div className="img">
+				<div className={ icon ? "img" : "img pfp"}>
 					{icon || <img
 						src="https://cdn.discordapp.com/avatars/492543441826349066/42c165aa65a0695ad140866b425bf9c3.webp?size=48"
 						alt=""
 					/>}
+                    <div className="appearance" data-appearance={appearance}></div>
 				</div>
-				<div className="name">{text || "He is spiderman"}</div>
+				<div className="name">{text}</div>
 			</li>
 		);
 }
