64402f5bb5c808f45a3f08286a64230364bb4370
glutonium69 <tahzib404@gmail.com>
2023-11-28 00:09:27 +0600

    Making the code in LIstItem.jsx cleaner

--- src/Components/Dm/List/ListItem.jsx
+++ src/Components/Dm/List/ListItem.jsx
@@ -1,15 +1,20 @@
 export default function ListItems({icon, text, appearance}) {
 
+    let img = (
+			<img
+				src="https://cdn.discordapp.com/avatars/492543441826349066/42c165aa65a0695ad140866b425bf9c3.webp?size=48"
+				alt=""
+			/>
+		);
+
     return (
 			<li>
 				<div className={ icon ? "img" : "img pfp"}>
-					{icon || <img
-						src="https://cdn.discordapp.com/avatars/492543441826349066/42c165aa65a0695ad140866b425bf9c3.webp?size=48"
-						alt=""
-					/>}
+					{icon || img}
+
                     <div className="appearance" data-appearance={appearance}></div>
 				</div>
-				<div className="name">{text}</div>
+				<span className="name">{text}</span>
 			</li>
 		);
 }
