c2dc9141c82f1688a05dde32a145b30d6e3615f8
glutonium69 <tahzib404@gmail.com>
2023-11-28 00:02:38 +0600

    Making Personal.jsx code cleaner

--- src/Components/Dm/Personal/Personal.jsx
+++ src/Components/Dm/Personal/Personal.jsx
@@ -4,30 +4,49 @@ export default function Personal() {
 
     let userName = "glutonium";
     let status = "Don't read the status"
+
+
+    const icons = (
+        <div className="icons">
+            <div className="img">{unmute}</div>
+            <div className="img">{deafen}</div>
+            <div className="img">{settings}</div>
+        </div>
+    );
+
+    const profileInfo = (
+        <div className="profile-info">
+            <span className="user-name">
+                {userName.length <= 13 ? userName : userName.slice(0, 13) + "..."}
+            </span>
+            <span className="status">
+                {status.length <= 13 ? status : status.slice(0, 13) + "..."}
+            </span>
+        </div>
+    );
+
+    const imgPfp = (
+        <div className="img pfp">
+            <img
+                src="https://cdn.discordapp.com/avatars/845398823571947580/04e17494d7b24833f738bb09101f1a9e.webp?size=160"
+                alt=""
+            />
+            <div className="appearance" data-appearance="offline"></div>
+        </div>
+    );
+
+    const profile = (
+        <a href="#" className="profile">
+            {imgPfp}
+            {profileInfo}
+        </a>
+    );
+
+
     return (
-			<div className="personal">
-				<a href="#" className="profile">
-					<div className="img pfp">
-						<img
-							src="https://cdn.discordapp.com/avatars/845398823571947580/04e17494d7b24833f738bb09101f1a9e.webp?size=160"
-							alt=""
-						/>
-						<div className="appearance" data-appearance="offline"></div>
-					</div>
-					<div className="profile-info">
-						<span className="user-name">
-                            {userName.length <= 13 ? userName : userName.slice(0,13) + "..."}
-                        </span>
-						<span className="status">
-                            {status.length <= 13 ? status : status.slice(0,13) + "..."}
-                        </span>
-					</div>
-				</a>
-				<div className="icons">
-					<div className="img">{unmute}</div>
-					<div className="img">{deafen}</div>
-					<div className="img">{settings}</div>
-				</div>
-			</div>
-		);
+        <div className="personal">
+            {profile}
+            {icons}
+        </div>
+    );
 }
