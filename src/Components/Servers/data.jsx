d3d857532cc2843c421e6f25436a52392a773c2f
glutonium69 <tahzib404@gmail.com>
2023-11-28 07:05:02 +0600

    Added a mock data for servers

---
+++ src/Components/Servers/data.jsx
@@ -0,0 +1,79 @@
+const servers = [
+	{
+		id: 1,
+		serverName: "Mystic Guardians",
+		icon: "//unsplash.it/seed/34/500/250",
+	},
+	{
+		id: 2,
+		serverName: "Eternal Legends",
+		icon: "//unsplash.it/seed/42/500/250",
+	},
+	{
+		id: 3,
+		serverName: "Galactic Warriors",
+		icon: "//unsplash.it/seed/38/500/250",
+	},
+	{
+		id: 4,
+		serverName: "Celestial Explorers",
+		icon: "//unsplash.it/seed/45/500/250",
+	},
+	{
+		id: 5,
+		serverName: "Inferno Knights",
+		icon: "//unsplash.it/seed/31/500/250",
+	},
+	{
+		id: 6,
+		serverName: "Abyssal Crew",
+		icon: "//unsplash.it/seed/48/500/250",
+	},
+	{
+		id: 7,
+		serverName: "Nebula Nomads",
+		icon: "//unsplash.it/seed/37/500/250",
+	},
+	{
+		id: 8,
+		serverName: "Radiant Alliance",
+		icon: "//unsplash.it/seed/41/500/250",
+	},
+	{
+		id: 9,
+		serverName: "Eclipse Empire",
+		icon: "//unsplash.it/seed/30/500/250",
+	},
+	{
+		id: 10,
+		serverName: "Phoenix Legion",
+		icon: "//unsplash.it/seed/44/500/250",
+	},
+	{
+		id: 11,
+		serverName: "Stellar Society",
+		icon: "//unsplash.it/seed/46/500/250",
+	},
+	{
+		id: 12,
+		serverName: "Supernova Squad",
+		icon: "//unsplash.it/seed/36/500/250",
+	},
+	{
+		id: 13,
+		serverName: "Cosmic Brotherhood",
+		icon: "//unsplash.it/seed/32/500/250",
+	},
+	{
+		id: 14,
+		serverName: "Astral Allies",
+		icon: "//unsplash.it/seed/49/500/250",
+	},
+	{
+		id: 15,
+		serverName: "Lunar Legends",
+		icon: "//unsplash.it/seed/35/500/250",
+	},
+];
+
+export { servers };
