f8904f301da5448e8055edcb2dd5585d1448fff2
glutonium69 <tahzib404@gmail.com>
2023-11-30 22:59:02 +0600

    Making the emoji change in chat input on hover

--- src/Components/Main/InputField/InputField.jsx
+++ src/Components/Main/InputField/InputField.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import { Media, Gift, Gif, Sticker, Emojis } from "./Icons"
 
 const otherIcons = [Gift, Gif, Sticker, Emojis];
 
 export default function InputField() {
 
-    const elements = 
-        otherIcons.map((icon, idx) => (
+    const [randomEmoji, setRandomEmoji] = useState(Emojis[Math.floor(Math.random() * Emojis.length)])
 
-			<div className={icon != Emojis ? "img" : "img emoji"} key={idx}>
+    function handleEmojiHover(){
+        setRandomEmoji(Emojis[Math.floor(Math.random() * Emojis.length)])
+    }
 
-				{icon != Emojis
-					? icon
-					: icon[Math.floor(Math.random() * Emojis.length)]}
-
+    const elements = otherIcons.map((icon, idx) => (
+			<div
+				className={icon != Emojis ? "img" : "img emoji"}
+				key={idx}
+				onMouseOver={handleEmojiHover}
+			>
+				{icon != Emojis ? icon : randomEmoji}
 			</div>
 		));
 
