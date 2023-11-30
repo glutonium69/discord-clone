a4ecc11e19437f51b8b51794e24029cf8934f2d7
glutonium69 <tahzib404@gmail.com>
2023-11-30 20:52:05 +0600

    Setting up Main InputField.jsx

--- src/Components/Main/InputField/InputField.jsx
+++ src/Components/Main/InputField/InputField.jsx
@@ -1,6 +1,26 @@
-export default function InputFeild() {
+import { Media, Gift, Gif, Sticker, Emojis } from "./Icons"
+
+const otherIcons = [Gift, Gif, Sticker, Emojis];
+
+export default function InputField() {
+
+    const elements = 
+        otherIcons.map((icon, idx) => (
+
+			<div className={icon != Emojis ? "img" : "img emoji"} key={idx}>
+
+				{icon != Emojis
+					? icon
+					: icon[Math.floor(Math.random() * Emojis.length)]}
+
+			</div>
+		));
 
     return (
-        <div className="input-feild"></div>
+        <div className="input-field">
+            <div className="img">{Media}</div>
+            <input type="text" placeholder="Message He Is Spiderman" />
+            {elements}
+        </div>
     );
 }
