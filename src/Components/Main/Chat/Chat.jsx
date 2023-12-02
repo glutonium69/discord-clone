62cad036c60e3d1fb1164bfbcd351db9985e5e9a
glutonium69 <tahzib404@gmail.com>
2023-12-03 02:30:23 +0600

    Importing Chat components

--- src/Components/Main/Chat/Chat.jsx
+++ src/Components/Main/Chat/Chat.jsx
@@ -1,6 +1,14 @@
+import ChatBeginChannel from "./ChatBeginChannel";
+import ChatBeginDm from "./ChatBeginDm";
+import MessageBody from "./Message";
+
 export default function Chat() {
 
+
     return (
-        <div className="chats"></div>
+        <div className="chats">
+            <ChatBeginDm />
+            <MessageBody />
+        </div>
     );
 }
