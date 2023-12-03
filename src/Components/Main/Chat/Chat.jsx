ff110704009d84be2e1e842cee0509dde8d969bb
glutonium69 <tahzib404@gmail.com>
2023-12-04 04:32:06 +0600

    Completed styling Chat.jsx

--- src/Components/Main/Chat/Chat.jsx
+++ src/Components/Main/Chat/Chat.jsx
@@ -1,13 +1,37 @@
 import ChatBeginChannel from "./ChatBeginChannel";
 import ChatBeginDm from "./ChatBeginDm";
 import MessageBody from "./Message";
+import { useEffect } from "react";
 
 export default function Chat() {
+
+    // useEffect(() => {
+
+    //     function setLength(){
+
+    //         const chat = document.querySelector(".chats");
+    //         const header = document.querySelector(".main .header").getBoundingClientRect().height;
+    //         const inputField = document.querySelector(".main .input-field").getBoundingClientRect().height;
+    
+    //         chat.style.height = window.innerHeight - header - inputField + "px";
+    //     }
+
+    //     // setLength();
+
+    //     window.addEventListener("resize",setLength)
+    //     return () => window.removeEventListener("resize", setLength)
+
+    // },[]);
+
 
 
     return (
         <div className="chats">
             <ChatBeginDm />
+            <MessageBody />
+            <MessageBody />
+            <MessageBody />
+            <MessageBody />
             <MessageBody />
         </div>
     );
--- src/Components/Main/Chat/Message.jsx
+++ src/Components/Main/Chat/Message.jsx
@@ -2,11 +2,27 @@ export default function MessageBody() {
 
     return (
         <div className="message-body">
-            <SenderInfo />
-            <Message />
+
+            <SenderPfp />
+
+            <div className="message-info">
+                <SenderInfo />
+                <Message />
+            </div>
         </div>
     );
 }
+
+const SenderPfp = () => (
+	<div className="img-holder">
+		<div className="img">
+			<img
+				src="https://cdn.discordapp.com/avatars/845398823571947580/04e17494d7b24833f738bb09101f1a9e.webp?size=160"
+				alt=""
+			/>
+		</div>
+	</div>
+);
 
 const SenderInfo = () => {
 
@@ -19,19 +35,14 @@ const SenderInfo = () => {
     let curr_minutes = date.getMinutes() >= 10 ? date.getMinutes() : `0${date.getMinutes()}`
 
     return(
-        <div className="sender-info">
-            <div className="img pfp">
-                <img
-                    src="https://cdn.discordapp.com/avatars/845398823571947580/04e17494d7b24833f738bb09101f1a9e.webp?size=160"
-                    alt=""
-                />
-            </div>
-            <span>glutonium</span>
-            <span>{`${curr_month}/${curr_date}/${curr_year}  ${curr_hours}:${curr_minutes}`}</span>
+        <div className="sender-name-time">
+            <span className="sender-name">glutonium</span>
+            <span className="send-date">{`${curr_month}/${curr_date}/${curr_year}`}</span> 
+            <span className="send-time">{`${curr_hours}:${curr_minutes}`}</span>
         </div>
     )
 };
 
 const Message = () => (
-    <span className="message">Man... Why are you gae?</span>
+    <span className="message">nventore, ipsum explicabo accusamus nesciunt nobis illo unde laudantium, aliquam quas assumenda saepe? Temporibus fuga voluptas perspiciatis illo, neque sint accusantium pariatur quo necessitatibus, est itaque. Laboriosam voluptate, eligendi quos minima et enim temporibus qui consequuntur facere blanditiis autem perspiciatis saepe rerum! Pariatur voluptas placeat saepe perspiciatiss, est itaque. Laboriosam voluptate, eligendi quos minima et enim temporibus qui consequuntur facere blanditiis autem perspiciatis saepe rerum! Pariatur voluptas placeat saepe perspiciatiss, est itaque. Laboriosam voluptate, eligendi quos minima et enim temporibus qui consequuntur facere blanditiis autem perspiciatis saepe rerum! Pariatur voluptas placeat saepe perspiciatiss, est itaque. Laboriosam voluptate, eligendi quos minima et enim temporibus qui consequuntur facere blanditiis autem perspiciatis saepe rerum! Pariatur voluptas placeat saepe perspiciatis repudiandae quis cupiditate. Quas exercitationem assumenda cupiditate officiis aliquam dicta eligendi libero, blanditiis hic quasi aut maiores molestias? Totam, ut voluptatem excepturi tempore deserunt recusandae! Blanditiis facilis delectus veniam voluptatibus earum saepe odit, voluptates recusandae amet neque esse? Necessitatibus nisi ea facere rem culpa, eveniet autem voluptates, s, est itaque. Laboriosam voluptate, eligendi quos minima et enim temporibus qui consequuntur facere blanditiis autem perspiciatis saepe rerum! Pariatur voluptas placeat saepe perspiciatisducimus in deserunt eligendi vel, asperiores voluptas.</span>
 )
