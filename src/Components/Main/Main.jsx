29f356cac16f8413b7f65ffe643c0c0459f50968
glutonium69 <tahzib404@gmail.com>
2023-12-04 19:40:10 +0600

    Further modification to chat - making the chat body scroll up when a new chat is send - gap and padding added to chat body

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -22,12 +22,9 @@ export default function Main() {
         }))
     }
 
-
     const [msgArr, setMsgArr] = useState([]);
 
-    function click(e){
-
-        if(e.key != "Enter" || chatInput.main__chat_input === "") return;
+    function handlePost(){
 
         setMsgArr( pre => [...pre, chatInput.main__chat_input])
 
@@ -37,12 +34,34 @@ export default function Main() {
         }))
     }
 
+    function scrollChatUp(){
+        const chatBody = document.querySelector(".chats");
+
+        const chatBodyGapValue = window
+					.getComputedStyle(chatBody)
+					.getPropertyValue("gap");
+
+        const messageBodyHeight = document.querySelector(".message-body:last-of-type").getBoundingClientRect().height;
+
+        const howMuchToScroll = messageBodyHeight + parseFloat(chatBodyGapValue);
+        
+        chatBody.scrollBy(0, howMuchToScroll);
+    }
+
+    function callFunctions(e){
+        if (e.key != "Enter" || chatInput.main__chat_input === "") return;
+        if( chatInput.main__chat_input === "clear" ) { setMsgArr([]); return } // temp code to clear the chat so i dont need to refresh the page every time
+        
+        handlePost()
+        setTimeout(scrollChatUp,0);
+    }
+
     useEffect(() => {
 
-        window.addEventListener("keydown", click)
+        window.addEventListener("keydown", callFunctions)
 
         return () => {
-            window.removeEventListener("keydown", click)
+            window.removeEventListener("keydown", callFunctions)
         }
     })
 
