deef76272df5f39a1092ed7c28ac734eed097706
glutonium69 <tahzib404@gmail.com>
2023-12-11 03:09:27 +0600

    Reformatting the code in handleChatAndChatBody.jsx

--- src/Components/Main/handleChatAndChatBody.jsx
+++ src/Components/Main/handleChatAndChatBody.jsx
@@ -5,54 +5,13 @@ const handleChatAndChatBody = (chatInput, setChatInput) => {
 
 	const [msgArr, setMsgArr] = useState([]);
 
-    // updates the msgArr and sets the current input value to ""
-	function handlePost() {
-
-        const { currDateFull, currTimeFull } = getTimeAndDate()
-        
-		setMsgArr((pre) => [
-			...pre,
-			{
-				id: 0,
-				timeDate: {
-					time: currTimeFull,
-					date: currDateFull,
-				},
-				msg: chatInput.main__chat_input,
-			},
-		]);
-
-        // sets the current input value to ""
-		setChatInput((preChatInput) => ({
-			...preChatInput,
-			main__chat_input: "",
-		}));
-	}
-
-    // every msg takes up some space and there's a defined gap between every two msgs
-    // this function makes sure when a msg is sent the element holding all chat is shifted upward based on the gap and the space the latest msg takes
-	function scrollChatUp() {
-		const chatBody = document.querySelector(".chats");
-
-		const chatBodyGapValue = window
-			.getComputedStyle(chatBody)
-			.getPropertyValue("gap");
-
-		const messageBodyHeight = document
-			.querySelector(".message-body:last-of-type")
-			.getBoundingClientRect().height;
-
-		const howMuchToScroll = messageBodyHeight + parseFloat(chatBodyGapValue);
-
-		chatBody.scrollBy(0, howMuchToScroll);
-	}
 
 	function callFunctions(e) {
 		if (e.key != "Enter" || chatInput.main__chat_input === "") return;
 
-		handlePost();
-        // makes sure that it runs after the msg in mounted in DOM
-        // so it can access the latest msg sent
+		handlePost(setMsgArr, chatInput);
+        emptyChatInput(setChatInput);
+        // makes sure that it runs after the msg in mounted in DOM so it can access the latest msg sent
 		setTimeout(scrollChatUp, 0);
 	}
 
@@ -66,5 +25,55 @@ const handleChatAndChatBody = (chatInput, setChatInput) => {
 
 	return { msgArr, setMsgArr };
 };
+
+
+// updates the msgArr with the new msg
+function handlePost(setMsgArr, chatInput) {
+
+    const { currDateFull, currTimeFull } = getTimeAndDate()
+    
+    setMsgArr((pre) => [
+        ...pre,
+        {
+            id: 0,
+            timeDate: {
+                time: currTimeFull,
+                date: currDateFull,
+            },
+            msg: chatInput.main__chat_input,
+        },
+    ]);
+}
+
+
+
+// sets the current input value to ""
+function emptyChatInput(setChatInput) {
+
+    setChatInput((preChatInput) => ({
+        ...preChatInput,
+        main__chat_input: "",
+    }));
+}
+
+
+// every msg takes up some space and there's a defined gap between every two msgs
+// this function makes sure when a msg is sent the element holding all chat is shifted upward based on the gap and the space the latest msg takes
+function scrollChatUp() {
+    const chatBody = document.querySelector(".chats");
+
+    const chatBodyGapValue = window
+        .getComputedStyle(chatBody)
+        .getPropertyValue("gap");
+
+    const messageBodyHeight = document
+        .querySelector(".message-body:last-of-type")
+        .getBoundingClientRect().height;
+
+    const howMuchToScroll = messageBodyHeight + parseFloat(chatBodyGapValue);
+
+    chatBody.scrollBy(0, howMuchToScroll);
+}
+
 
 export default handleChatAndChatBody;
