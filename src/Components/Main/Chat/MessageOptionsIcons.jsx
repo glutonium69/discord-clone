66f8d2ffdaa51b48068756ce4f4904968038e89c
glutonium69 <tahzib404@gmail.com>
2023-12-10 06:19:55 +0600

    Setting up icons for message options on hover

---
+++ src/Components/Main/Chat/MessageOptionsIcons.jsx
@@ -0,0 +1,59 @@
+const reactions = (
+	<svg
+		className="icon_bdb038"
+		aria-hidden="true"
+		role="img"
+		xmlns="http://www.w3.org/2000/svg"
+		width="20"
+		height="20"
+		fill="none"
+		viewBox="0 0 24 24"
+	>
+		<path
+			fill="currentColor"
+			fillRule="evenodd"
+			d="M12 23a11 11 0 1 0 0-22 11 11 0 0 0 0 22ZM6.5 13a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Zm11 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Zm-9.8 1.17a1 1 0 0 1 1.39.27 3.5 3.5 0 0 0 5.82 0 1 1 0 0 1 1.66 1.12 5.5 5.5 0 0 1-9.14 0 1 1 0 0 1 .27-1.4Z"
+			clipRule="evenodd"
+		></path>
+	</svg>
+);
+
+const edit = (
+	<svg
+		className="icon_d1ac81"
+		aria-hidden="true"
+		role="img"
+		xmlns="http://www.w3.org/2000/svg"
+		width="20"
+		height="20"
+		fill="none"
+		viewBox="0 0 24 24"
+	>
+		<path
+			fill="currentColor"
+			d="m13.96 5.46 4.58 4.58a1 1 0 0 0 1.42 0l1.38-1.38a2 2 0 0 0 0-2.82l-3.18-3.18a2 2 0 0 0-2.82 0l-1.38 1.38a1 1 0 0 0 0 1.42ZM2.11 20.16l.73-4.22a3 3 0 0 1 .83-1.61l7.87-7.87a1 1 0 0 1 1.42 0l4.58 4.58a1 1 0 0 1 0 1.42l-7.87 7.87a3 3 0 0 1-1.6.83l-4.23.73a1.5 1.5 0 0 1-1.73-1.73Z"
+		></path>
+	</svg>
+);
+
+const settings = (
+	<svg
+		className="icon_d1ac81"
+		aria-hidden="true"
+		role="img"
+		xmlns="http://www.w3.org/2000/svg"
+		width="20"
+		height="20"
+		fill="none"
+		viewBox="0 0 24 24"
+	>
+		<path
+			fill="currentColor"
+			fillRule="evenodd"
+			d="M4 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm10-2a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm8 0a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
+			clipRule="evenodd"
+		></path>
+	</svg>
+);
+
+export { reactions, edit, settings }
